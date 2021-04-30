ff10be82842c87a66228079f1312f1c92ccf8c14
Billy Sjöberg <billy.sjoberg@gmail.com>
2021-04-30 14:44:03 +0200

    added script for starting JFR profiling

---
+++ Profile.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+public class Profile {
+    private static final Path LOG_FILE_NAME = Paths.get("log.jfr").toAbsolutePath();
+    private static final Path CONFIG_FILE_NAME = Paths.get("config.jfc").toAbsolutePath();
+    private static final String PROFILING_NAME = "temp";
+    private static final Duration DEFAULT_DURATION = Duration.ofSeconds(30);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length < 1) {
+            exit(1, "expected pid to profile");
+        }
+
+        var duration = args.length == 2 ?
+                Duration.ofSeconds(Integer.parseInt(args[1])) :
+                DEFAULT_DURATION;
+
+        final int pid = Integer.parseInt(args[0]);
+
+        run("jcmd %d JFR.start name=%s settings=%s".formatted(pid, PROFILING_NAME, CONFIG_FILE_NAME));
+        println("Started profiling %d for %d seconds".formatted(pid, duration.getSeconds()));
+        final LocalTime started = LocalTime.now();
+
+        while (started.plus(duration).isAfter(LocalTime.now())) {
+            TimeUnit.SECONDS.sleep(1);
+            System.out.print(".");
+        }
+
+        println("Stopping profiling");
+        run("jcmd %d JFR.dump name=%s filename=%s".formatted(pid, PROFILING_NAME, LOG_FILE_NAME));
+        Runtime.getRuntime().exec("jcmd %d JFR.stop name=%s".formatted(pid, PROFILING_NAME));
+
+        println("Starting JavaFlames");
+        run("java JavaFlames.java %s".formatted(LOG_FILE_NAME));
+
+        Files.deleteIfExists(LOG_FILE_NAME);
+    }
+
+    private static void println(String s) {
+        System.out.println(s);
+    }
+
+    private static void run(String command) throws IOException, InterruptedException {
+        println("> " + command);
+        final int exitCode = new ProcessBuilder(command.split(" "))
+                .inheritIO()
+                .start()
+                .waitFor();
+
+        if (exitCode != 0) {
+            throw new IllegalStateException("Command \"%s\" exited with code %d".formatted(command, exitCode));
+        }
+    }
+
+    private static void exit(int code, String message) {
+        println(message);
+        usage();
+        System.exit(code);
+    }
+
+    private static void usage() {
+        println("Usage: java Profile.java <pid> [<duration in seconds>]");
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Flamegraph from JFR logs
-Simple one file java script to generate flamegraphs from Java flight recordings without requiring to install Perl and the [Brendan Gregg scripts](https://github.com/brendangregg/FlameGraph).
+Simple one file Java script to generate flamegraphs from Java flight recordings without installing Perl and the [Brendan Gregg scripts](https://github.com/brendangregg/FlameGraph).
 
+It also comes with a separate script to start profiling a java process using FlightRecorder and then call the flamegraph script. 
 ## Usage
 With a fairly recent Java version installed simply run:
 `java JavaFlames.java <path to jfr recording file>`
 
 This launches an http server serving the `index.html` file and endpoint for the folded format extracted from the JFR logs.
-After that the script opens up a browser showing the results.
+The script then opens up a browser showing the results.
+
+Alternatively if you don't have an existing JFR recording you can also start the `Profile.java` script to start profiling a java process generating the JFR file and then triggering flamegraph generation.
+
+`java Profile.java <pid> [<duration in seconds>]`
+
+The default profiling duration unless specified is 30 seconds.
 
 ## Credits
 Brendan Gregg for introducing the concept of flamegraphs and https://github.com/spiermar/d3-flame-graph for the javascript/html implementation.
