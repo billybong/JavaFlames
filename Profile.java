398b6a8ff267edd1284313bd2479d80f185ec701
Billy Sjöberg <billy.sjoberg@gmail.com>
2021-05-05 13:07:05 +0200

    Lookup pid to profile from process name. Takes care of doing the roundtrip "jps -> copy/paste pid -> java Profile.java <pid>"

--- Profile.java
+++ Profile.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,25 +11,34 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Profile {
-    private static final String now = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
-    private static final Path LOG_FILE_NAME = Paths.get("log" + now + ".jfr").toAbsolutePath();
+    private static final String NOW = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+    private static final Path LOG_FILE_NAME = Paths.get("log" + NOW + ".jfr").toAbsolutePath();
     private static final Path CONFIG_FILE_NAME = Paths.get("config.jfc").toAbsolutePath();
     private static final String PROFILING_NAME = "temp";
     private static final Duration DEFAULT_DURATION = Duration.ofSeconds(30);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
+        try {
+            run(args);
+        } catch (Exception throwable) {
+            throwable.printStackTrace();
+            exit(99, "Encountered exception: " + throwable.getMessage());
+        }
+    }
+
+    private static void run(String[] args) throws IOException, InterruptedException {
         if (args.length < 1) {
             exit(1, "expected pid to profile");
         }
+
+        final int pid = findProcessPid(args[0]);
 
         var duration = args.length == 2 ?
                 Duration.ofSeconds(Integer.parseInt(args[1])) :
                 DEFAULT_DURATION;
 
-        final int pid = Integer.parseInt(args[0]);
-
         run("jcmd %d JFR.start name=%s settings=%s".formatted(pid, PROFILING_NAME, CONFIG_FILE_NAME));
-        println("Started profiling %d for %d seconds".formatted(pid, duration.getSeconds()));
+        println("Started profiling process %d for %d seconds".formatted(pid, duration.getSeconds()));
         final LocalTime started = LocalTime.now();
 
         while (started.plus(duration).isAfter(LocalTime.now())) {
@@ -43,6 +54,25 @@ public class Profile {
         run("java JavaFlames.java %s".formatted(LOG_FILE_NAME));
 
         Files.deleteIfExists(LOG_FILE_NAME);
+    }
+
+    private static int findProcessPid(String javaProcess) throws IOException {
+        if(javaProcess.matches("[0-9]+")){
+            return Integer.parseInt(javaProcess);
+        }
+
+        final Process jps = new ProcessBuilder("jps").start();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(jps.getInputStream()))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.endsWith(javaProcess)) {
+                    return Integer.parseInt(line.split(" ")[0]);
+                }
+            }
+        }
+
+        throw new IllegalStateException("Failed to find pid for process: " + javaProcess);
     }
 
     private static void println(String s) {
